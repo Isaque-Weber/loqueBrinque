58f79a2f5c20d20445701ee280c2890239bdc8f6
Isaque Weber <contato@thiagomoraes.com.br>
2024-08-10 12:56:44 -0300

    refactor

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,15 +2,6 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
         const target = document.querySelector(this.getAttribute('href'));
         const offset = 100; // Ajuste conforme necessário para a altura do cabeçalho
@@ -21,18 +12,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-const hamburger = document.getElementById('hamburger');
-    const closeMenu = document.getElementById('close-menu');
-    const menuItems = document.querySelector('nav ul');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 100; // Ajuste conforme necessário para a altura do cabeçalho
 
-    hamburger.addEventListener('click', function () {
-        menuItems.classList.add('show');
-        hamburger.style.display = 'none';
-        closeMenu.style.display = 'block';
+        window.scrollTo({
+            top: target.offsetTop - offset,
+            behavior: 'smooth'
+        });
     });
-
-    closeMenu.addEventListener('click', function () {
-        menuItems.classList.remove('show');
-        hamburger.style.display = 'block';
-        closeMenu.style.display = 'none';
-    });
+});
