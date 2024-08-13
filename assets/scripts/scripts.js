1d882d2eacb6d5b0299bbe584786fc98d82217ce
Isaque Weber <isaqueweber@hotmail.com>
2024-08-13 17:15:04 -0300

    Refactor

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -12,15 +12,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        const offset = 100; // Ajuste conforme necessário para a altura do cabeçalho
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const closeMenu = document.querySelector('.close-menu');
+    const navMenu = document.querySelector('nav ul');
 
-        window.scrollTo({
-            top: target.offsetTop - offset,
-            behavior: 'smooth'
-        });
+    hamburger.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+
+    closeMenu.addEventListener('click', function() {
+        navMenu.classList.remove('show');
     });
 });
